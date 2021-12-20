747c36c4433a9b6e177f42190780eea9cd0ac65e
Bruno Santos <brunobsantos.04@gmail.com>
2021-12-20 18:11:58 +0000

    update DB

---
+++ API/Model/Event/EventDB.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Model
+{
+    public class EventDB
+    {
+        public int Id { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+
+        public float Home_Odd { get; set; }
+        public float Tie_Odd { get; set; }
+        public float Away_Odd { get; set; }
+
+        // Table EventType
+        public EventType eventType { get; set; }
+        public int eventTypeId { get; set; }
+
+        // Table Sport
+        public Sport sport { get; set; }
+        public int sportId { get; set; }
+
+    }
+}
