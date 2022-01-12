a45d726e3876ab360851ec2bb51f062fe30cde71
Bruno Santos <brunobsantos.04@gmail.com>
2022-01-12 16:53:23 +0000

    up BD

--- API/Migrations/20220112165225_InitialCreate.Designer.cs
+++ API/Migrations/20220112165225_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20220112155603_InitialCreate")]
+    [Migration("20220112165225_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
